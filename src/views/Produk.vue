dddcf7dee3529588e15bd1f0ecfd44e51a6b374c
Nazriilhamsyah <nazril_062@smkassalaambandung.sch.id>
2023-08-16 03:24:41 -0400

    latihan-produk-linda

--- src/views/Produk.vue
+++ src/views/Produk.vue
@@ -62,7 +62,7 @@
             </a>
             <a href="/singleproduk" class="group">
               <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img src="../assets/gambar7.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
+                <img src="../assets/gambar14.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
               </div>
               <h3 class="mt-4 text-sm text-gray-700">Casing Iphone</h3>
               <p class="mt-1 text-lg font-medium text-gray-900">Rp. 25.000 - Rp. 50.000</p>
